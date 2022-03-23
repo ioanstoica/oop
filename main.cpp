642095d65758dc06f3fc2f4703631ded20bfbc79
Stoica Ioan <stoicaioan2002@gmail.com>
2022-03-23 07:10:35 +0200

    Adaugare prima clasa.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <fstream>
+
+class candela{
+    public:
+        int OpenPrice;
+};
+
 
 int main() {
     std::cout << "Hello, world!\n";
+    std::fstream f;
+    f.open("date.txt",std::fstream::in);
+    int x,n=0,i;
+    candela v[100];
+    while(f>>x)
+        v[++n].OpenPrice = x;
+    for (i=1;i<=n;i++)
+        std::cout<<v[i].OpenPrice<<std::endl;
+
+
+    f.close();
+
     return 0;
 }
