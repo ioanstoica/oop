951ca2f38ae77fe81d3d93bcebce5585268f61b6
Stoica Ioan <stoicaioan2002@gmail.com>
2022-06-17 06:41:17 +0300

    ?

--- include/candela.h
+++ include/candela.h
@@ -16,23 +16,11 @@ public:
     }
     timp getData();
     void setOpen(double Open_);
-    void setData(timp Data_)
-    {
-        Data = Data_;
-    };
+    void setData(timp Data_);
     candela(const candela& other) : Data(other.Data), Open(other.Open), High(other.High), Low(other.Low), Volume(other.Volume)  {}
     candela(timp Data_, double Open_, double High_, double Low_, double Close_, double Volume_) : Data{Data_}, Open{Open_}, High(High_), Low(Low_), Close(Close_), Volume(Volume_) {}
     candela(timp Data_, double Open_) : Data{Data_}, Open{Open_} {}
-    candela& operator=(const candela& other)
-    {
-        Data = other.Data;
-        Open = other.Open;    //operator =
-        High = other.High;
-        Low = other.Low;
-        Close = other.Close;
-        Volume = other.Volume;
-        return *this;
-    }
+    candela& operator=(const candela& other);
     ~candela() {/*std::cout << "Destr candela\n";*/}// destructor
     friend std::ostream& operator<<(std::ostream& os, const candela& st)
     {
--- src/candela.cpp
+++ src/candela.cpp
@@ -14,3 +14,19 @@ void candela::setOpen(double Open_)
 {
     Open = Open_;
 }
+
+void candela::setData(timp Data_)
+{
+    Data = Data_;
+};
+
+candela& candela::operator=(const candela& other)
+    {
+        Data = other.Data;
+        Open = other.Open;    //operator =
+        High = other.High;
+        Low = other.Low;
+        Close = other.Close;
+        Volume = other.Volume;
+        return *this;
+    }
